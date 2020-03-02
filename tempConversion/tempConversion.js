f85b6cac8eed04b92bebe309bec21a4c71b421ef
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2020-03-02 14:28:05 +0700

    add tempConversion solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let result = (temp-32) * 5/9;
+  result = Number.isInteger(result) ? result : parseFloat(result.toFixed(1));
 
+  return (result);
 }
 
-const ctof = function() {
+const ctof = function(temp) {
+  let result = (temp * 9/5) + 32;
+  result = Number.isInteger(result) ? result : parseFloat(result.toFixed(1));
 
+  return (result);
 }
 
 module.exports = {
