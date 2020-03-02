ac23f8f3d7d418309188c73c102a756f0dee84e1
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2020-03-02 14:09:47 +0700

    add solution for sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  let sum = 0;
+  if (min < 0 || max < 0 || typeof(min) !== 'number' || typeof(max) !== 'number') {
+    return ("ERROR");
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  for(let i = min; i < max + 1; i++) {
+    sum += i;
+  }
 
+  return sum;
 }
 
 module.exports = sumAll
